c55f7ce09100bd0b358c9c03d0bbf22468212fb8
Mohamed Benaichouche <benaichouchem@gmail.com>
2020-11-30 10:54:04 +0100

    removed unused code

--- app.js
+++ app.js
@@ -2,35 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-async function GetSecretWithName(secretName) {
-    
-    const name = secretName;
-      
-    // Imports the Secret Manager library
-    const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-    
-    // Instantiates a client
-    const client = new SecretManagerServiceClient();
-    try {
-        const [secretVersion] = await client.accessSecretVersion({
-            name: name,
-        }); 
-
-        const payload = secretVersion.payload.data.toString();
-        return payload;
-
-    }
-    catch (e) {
-        console.log(e, "ERROR");
-
-    }   
-  }
-
 
 // Defining our application routes
 var indexRouter = require('./routes/index');
 var dataRouter = require('./routes/getData');
-const {GoogleSpreadsheet} = require("google-spreadsheet");
 
 var app = express();
 
@@ -46,26 +21,9 @@ app.use('/', indexRouter);
 console.log('getData');
 app.use('/getData', dataRouter);
 
-app.get("/google-spreadsheet", async function(req, res){
-    // Identifying which document we'll be accessing/reading from
-    var dataSecret = await GetSecretWithName('projects/a7mini/secrets/MasterDataSheet/versions/2');
-
-    const doc = new GoogleSpreadsheet(dataSecret);
-
-    // Authentication
-    await doc.useServiceAccountAuth(creds);
-
-    await doc.loadInfo(); // loads document properties and worksheets
-    console.log(doc.title);
-    // await doc.updateProperties({ title: 'renamed doc' });
-
-    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-    console.log(sheet.title);
-    console.log(sheet.rowCount);
-    const rows = await sheet.getRows();
-    res.send(rows[0].worldDeaths);
-});
-
+// app.get("/api/members", (req, res) => {
+//     res.send("answer");
+// });
 
 
 module.exports = app;
